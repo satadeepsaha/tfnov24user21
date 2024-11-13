c33b703671ab84d0fa73408ca548467747094a57
satadeep <satadeep@gmail.com>
2024-11-13 17:35:33 +0530

    3rd commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "vpc21" {
-    cidr_block = "10.111.0.0/16"
+    cidr_block = "10.21.0.0/16"
     tags ={
-        Name = "vpc from cli to HCPCloud"
+        Name = "vpc21 from cli to HCPCloud"
     }
   
 }
